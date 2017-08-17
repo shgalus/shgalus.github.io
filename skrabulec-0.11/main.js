c47f96aade516009d5725a53b0d4ccba039b6163
Stanisław Galus <shgalus@gmail.com>
2017-08-17 20:38:45 +0200

    skrabulec v0.11 replaced skrabulec v0.1.

---
+++ skrabulec-0.11/main.js
@@ -0,0 +1,36 @@
+SKRABULEC.main = (function() {
+  "use strict";
+  var version, initModule;
+
+  version = "0.11";
+
+  initModule = function(container, option) {
+    var conf, dict;
+    if (option === "en") {
+      conf = SKRABULEC.conf.en.config_map;
+      dict = SKRABULEC.dict.en;
+    } else if (option === "pl") {
+      conf = SKRABULEC.conf.pl.config_map;
+      dict = SKRABULEC.dict.pl;
+    } else if (option === "us") {
+      conf = SKRABULEC.conf.en.config_map;
+      dict = SKRABULEC.dict.us;
+    } else
+      SKRABULEC.utils.assert(false);
+    SKRABULEC.engine.initModule(conf, dict);
+    SKRABULEC.ui.initModule(container, conf);
+    SKRABULEC.testing.doAllTests();
+    SKRABULEC.ui.startGame();
+  };
+
+  return {
+    varsion: version,
+    initModule: initModule
+  };
+}());
+
+$(document).ready(function() {
+  "use strict";
+  SKRABULEC.settings.getSettings(jQuery('#scrabble'),
+                                 SKRABULEC.main.initModule);
+});
